97cb4de687fe473ae8dc9bae651eab98024ae5a5
vineetbadoni <vineetbadoni@gmail.com>
2015-10-11 12:28:34 +0530

    HelloWorld Openshift

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /**
  * Module dependencies.
- */
+ *//*
+
 
 var express = require('express')
     , routes = require('./server/app/routes/index')
@@ -69,4 +70,19 @@ app.get('/angularview', angularview.angularview);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
+});*/
+
+var http = require('http');
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
 });
