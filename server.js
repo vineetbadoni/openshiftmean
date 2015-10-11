910bb3d9535f2d90dbfeba44364c1c742fab974d
vineetbadoni <vineetbadoni@gmail.com>
2015-10-11 14:02:22 +0530

    Adding life to it

--- server.js
+++ server.js
@@ -72,8 +72,13 @@ http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });*/
 
-var http = require('http');
-var express = require('express');
+var express = require('express')
+    , routes = require('./server/app/routes/index')
+    , user = require('./server/app/routes/user')
+    , angularview = require('./server/app/routes/angularview')
+    , http = require('http')
+    , path = require('path');
+
 var app = express();
 
 var MongoClient = require('mongodb').MongoClient;
@@ -102,6 +107,36 @@ MongoClient.connect(dbURL, function (err, database) {
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
+app.set('views', __dirname + '/client/public/views/app');
+
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use('/public', express.static(path.join(__dirname, '/client/public')));
+
+console.log(path.join(__dirname, '/client/public'));
+//Hooking up the GUIs
+
+//This enables to use ejs files as views
+//app.set('view engine', 'ejs');
+
+//This enables the .html files are views
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+//Hooking up the GUIs
+
+// Development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.get('/angularview', angularview.angularview);
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
     console.log('Express server listening on port ' + app.get('port'));
